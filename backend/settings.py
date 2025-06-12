acd5ebb5b3416f4c07b6a2c135e00beb87340152
sdCarr <sarerac@gmail.com>
2025-06-12 22:30:16 +0200

    Initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# forzar depluy en render
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
