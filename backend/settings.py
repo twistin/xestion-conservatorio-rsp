d4f541c7b7bd31d3eef6173bb09f84bfe6371903
sdCarr <sarerac@gmail.com>
2025-06-12 13:19:18 +0200

    Initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -126,3 +126,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
