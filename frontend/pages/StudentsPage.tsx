3c2aa1946f416c0a9c091b2985876444fd008271
sdCarr <sarerac@gmail.com>
2025-06-14 23:56:01 +0200

    Initial commit

--- frontend/pages/StudentsPage.tsx
+++ frontend/pages/StudentsPage.tsx
@@ -318,7 +318,10 @@ const StudentsPage: React.FC = () => {
                 {/* Botones de acción */}
                 <div className="flex justify-end mt-4">
                   <Button variant="danger" onClick={() => handleDeleteStudent(studentDetails.id)}>Dar de baixa</Button>
-                  <Button variant="primary" className="ml-2" onClick={() => handleEditStudent(studentDetails)}>Editar</Button>
+                  <Button variant="primary" className="ml-2" onClick={() => {
+                    setShowFichaModal(false);
+                    handleEditStudent(studentDetails);
+                  }}>Editar</Button>
                 </div>
               </>
             )}
