e1a417a7bd83343c90aafbfe995e0dd9c21ca922
sdCarr <sarerac@gmail.com>
2025-06-14 23:24:27 +0200

    Initial commit

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student, Instrument, Professor, Course, Enrollment
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Instrument)
+admin.site.register(Professor)
+admin.site.register(Course)
+admin.site.register(Enrollment)
